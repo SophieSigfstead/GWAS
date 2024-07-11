7892d4f16c3548c5dbf12511bfc8a535394c8ef5
SophieSigfstead <72667266+SophieSigfstead@users.noreply.github.com>
2024-07-11 17:08:21 +0200

    enformer pytorch attempt

--- enformer_test_run/tangermeme_test.py
+++ enformer_test_run/tangermeme_test.py
@@ -2,6 +2,7 @@ from enformer_pytorch import from_pretrained, seq_indices_to_one_hot
 import torch
 import tangermeme
 from tangermeme.deep_lift_shap import deep_lift_shap
+from matplotlib import pyplot as plt
 
 def main():
 
@@ -15,9 +16,20 @@ def main():
 
     print(pred)
 
-    X_attr = deep_lift_shap(model, seq, target='human', random_state=0)
+    X_attr = deep_lift_shap(model, one_hot, target='human', random_state=0)
 
     print(X_attr.shape)
+
+    # Visualize attributions
+    plt.figure(figsize=(10, 2))
+    ax = plt.subplot(111)
+    plot_logo(X_attr[0, :, 950:1050], ax=ax)
+
+    plt.xlabel("Genomic Coordinate")
+    plt.ylabel("Attributions")
+    plt.title("DeepLIFT Attributions for Human")
+    plt.ylim(-0.05, 0.35)
+    plt.show()
 
     return
 
