04b23b22a5c5ab5042975739704215d0d4e2f2cf
SophieSigfstead <72667266+SophieSigfstead@users.noreply.github.com>
2024-07-11 17:03:52 +0200

    enformer pytorch attempt

--- enformer_test_run/tangermeme_test.py
+++ enformer_test_run/tangermeme_test.py
@@ -1,5 +1,7 @@
 from enformer_pytorch import from_pretrained, seq_indices_to_one_hot
 import torch
+import tangermeme
+from tangermeme.deep_lift_shap import deep_lift_shap
 
 def main():
 
@@ -13,7 +15,12 @@ def main():
 
     print(pred)
 
+    X_attr = deep_lift_shap(model, seq, target='human', random_state=0)
+
+    print(X_attr.shape)
+
     return
+
 
 if __name__ == "__main__":
     main()
