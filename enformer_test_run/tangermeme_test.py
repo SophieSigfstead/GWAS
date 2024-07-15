553642c88fda3bfb9c7bfc43890166f4c77a279e
SophieSigfstead <72667266+SophieSigfstead@users.noreply.github.com>
2024-07-15 10:36:25 +0200

    working on using tangermeme

--- enformer_test_run/tangermeme_test.py
+++ enformer_test_run/tangermeme_test.py
@@ -22,13 +22,17 @@ def main():
     print(pred)
 
     X = random_one_hot((1,4,2000)).type(torch.float32)
+
     #X = substitute(X, "GTGACTCATC")# Ensure indices are in the range [0, 3]
     X = X.permute(0,2,1)
 
+    print("X shape after permutation", X.shape)
+
     print(X)
+
     X_attr = deep_lift_shap(model, X, target='human', random_state=0)
 
-    print(X_attr.shape)
+    #print(X_attr.shape)
 
     # Visualize attributions
     plt.figure(figsize=(10, 2))
