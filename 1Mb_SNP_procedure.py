56844be22e4242420168f5656f7bedf370be4267
Sophie Sigfstead <ssigfstead@lrabuzin-pc.inf.ethz.ch>
2024-07-18 14:59:31 +0200

    Adding scripts relating to 1Mb GWAS procedure

--- 1Mb_SNP_procedure.py
+++ 1Mb_SNP_procedure.py
@@ -1,55 +1,70 @@
 import pandas as pd
 import sys
 
-def main(path_to_combined_csv):
+def threshold_calculator(df, sd):
+    df_with_sad = df[df['SAD1'].notna()]
+    std_dev = df_with_sad['average_SAD'].std()
+    mean = df_with_sad['average_SAD'].mean()
+    upper_threshold = mean + (sd * std_dev)
+    lower_threshold = mean - (sd * std_dev)
+    return upper_threshold, lower_threshold
 
-    # filter the CSV file to only include SNPs with non-null average_SAD
+def main(path_to_combined_csv, sd):
+
+    sd = float(sd)
+
     df = pd.read_csv(path_to_combined_csv)
-    df = df[df['average_SAD'].notna()]
 
-    # create a csv file with the header 
+    UPPER_THRESHOLD, LOWER_THRESHOLD = threshold_calculator(df, sd)
+
+    print("Number of SD for threshold:", sd)
+
+    n_snps_prev = len(df)
+
+    df = df[df['SAD1'].notna()]
+
+    print('n_snps_prev:', n_snps_prev)
+
     output_columns = ['SAD1', 'SAD9', 'alt', 'chr', 'pos', 'ref', 'snp', 'beta', 'standard_error', 'effect_allele_frequency', 'p_value', 'average_SAD', 'abs_average_SAD', 'ranking']
     result_df = pd.DataFrame(columns=output_columns)
 
-    # set a threshold value, say THRESHOLD = 0.00002
-    THRESHOLD = 0.00000
+    n_snps_filtered = len(df[(df['average_SAD'] > UPPER_THRESHOLD) | (df['average_SAD'] < LOWER_THRESHOLD)])
+    print('n_snps_filtered:', n_snps_filtered)
 
-    # for the list of chromosomes in the tsv file (chr = 1 to 22), indicated by the column chr:
     for chr_num in range(1, 23):
         chr_df = df[df['chr'] == chr_num]
-        chr_df = chr_df[(chr_df['average_SAD'] > THRESHOLD) | (chr_df['average_SAD'] < (-1*THRESHOLD))]
-        chr_df = chr_df.sort_values(by='average_SAD', ascending=False).reset_index(drop=True)
-        
+        chr_df = chr_df[(chr_df['average_SAD'] > UPPER_THRESHOLD) | (chr_df['average_SAD'] < LOWER_THRESHOLD)]
+        chr_df = chr_df.sort_values(by='p_value', ascending=True).reset_index(drop=True)
+
         while not chr_df.empty:
-            # get the top ranked SNP for that chromosome based on the average_SAD value, provided it is greater than THRESHOLD or less than -THRESHOLD. Add the full row to a csv file.
             top_snp = chr_df.iloc[0]
             result_df = pd.concat([result_df, pd.DataFrame([top_snp], columns=output_columns)], ignore_index=True)
-            
-            # let position_of_sig_snp = pos of the row just added to the csv.
             pos_sig_snp = top_snp['pos']
-            
-            # go down the list for the current chromosome only, and eliminate any rows that have (pos < position_of_sig_snp + 1000000) or (pos > position_of_sig_snp - 1000000)
-            chr_df = chr_df[~((chr_df['pos'] < pos_sig_snp + 100000) & (chr_df['pos'] > pos_sig_snp - 100000))]
+            chr_df = chr_df[~((chr_df['pos'] < pos_sig_snp + 1000000) & (chr_df['pos'] > pos_sig_snp - 1000000))]
             chr_df = chr_df.reset_index(drop=True)
 
-    # save the csv file of the rows found in the above loop
-    result_df.to_csv('./filtered_snps.csv', index=False)
-    
-    # print out the length of the saved csv file
-    print(f"Length of the saved CSV file: {len(result_df)}")
-    
-    # using another file located in ./sorted_GWAS_summary_data/GCST90277450_sorted_significant.csv
-    other_file_path = './sorted_GWAS_summary_data/GCST90277450_sorted_significant.csv'
-    other_df = pd.read_csv(other_file_path, sep='\t')
-    
-    # print out how many rows share the same value for chr (or chromosome in the GCST90277450_sorted_significant.csv)
-    common_chr_count = len(set(result_df['chr']).intersection(set(other_df['chromosome'])))
-    print(f"Number of common chromosomes: {common_chr_count}")
+    new_p_value_threshold = 5e-8 * (n_snps_prev / n_snps_filtered)
+    print('new_p_value_threshold:', new_p_value_threshold)
+
+    result_df = result_df[result_df['p_value'] < new_p_value_threshold]
+
+    output_file = f'./filtered_snps_threshold={sd}.csv'
+    result_df.to_csv(output_file, index=False)
+
+    print(f"Length of the significant snps file: {len(result_df)}")
+
+    # Compare with mdd_sig_snps.csv
+    mdd_sig_snps = pd.read_csv('mdd_sig_snps.csv')
+    matching_snps = pd.merge(result_df, mdd_sig_snps, left_on=['chr', 'pos'], right_on=['chromosome', 'base_pair_location'])
+    print(f"Number of matching rows with mdd_sig_snps.csv: {len(matching_snps)}")
+
+    return
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <path for combined csv>")
+    if len(sys.argv) != 3:
+        print("Usage: python script.py <path to combined csv> <number of sd for threshold>")
     else:
         path_to_combined_csv = sys.argv[1]
-        main(path_to_combined_csv)
+        sd = sys.argv[2]
+        main(path_to_combined_csv, sd)
     sys.exit()
